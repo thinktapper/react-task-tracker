8e173828dea1b5cfd3f9d69ecf9c904c3da77320
Andrew Tapper <andrew@tapper.codes>
2022-07-04 09:57:21 -0500

    button onClick working

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types'
 
-const Button = ({ color, text }) => {
-  return <button style={{ backgroundColor: color }} className='btn'>{text}</button>
+const Button = ({ color, text, onClick }) => {
+  return <button onClick={onClick} style={{ backgroundColor: color }} className='btn'>{text}</button>
 }
 
 Button.defaultProps = {
@@ -11,6 +11,7 @@ Button.defaultProps = {
 Button.propTypes = {
     text: PropTypes.string,
     color: PropTypes.string,
+    onClick: PropTypes.func,
 }
 
 export default Button
